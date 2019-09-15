61b8bd36cb14adf097cf507588bf349d8998d382
卞晓冬 <83742368@qq.com>
2019-09-15 21:10:13 +0800

    java学习

---
+++ src/com/bianxiaodong/java/io/Demo.java
@@ -0,0 +1,19 @@
+package com.bianxiaodong.java.io;
+
+import java.io.File;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 输入流基类(抽象类) InputStream(字节) Reader(字符)
+		// 输出流基类(抽象类) OutputStream(字节) Writer(字符)
+		
+		// FileInputStream FileReader
+		// FileOutputStream FileWriter
+		
+		// 字节流转字符流 InputStreamReader OutputStreamWriter
+	}
+
+}
